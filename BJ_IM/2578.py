cb2a547e25671313ecd283a651e833ef4ecf25e4
yusoojeong <yusj1231@gmail.com>
2020-02-11 17:58:58 +0900

    200211 | -ing

---
+++ BJ_IM/2578.py
@@ -0,0 +1,13 @@
+
+data = [list(map(int, input().split())) for _ in range(5)]
+data2 = [list(map(int, input().split())) for _ in range(5)]
+
+call = []
+
+for i in range(5):
+    for j in range(5):
+        call.append(data2[i][j])
+
+
+
+
